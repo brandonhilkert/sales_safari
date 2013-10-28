65900d20823cf87ce4f4a338ca22293f4e2aaed9
Brandon Hilkert <brandonhilkert@gmail.com>
2013-10-28 11:39:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalesSafari::Application.config.secret_key_base = 'a7994ee4045291be8f4fc6cbb1e98338ecb7b62c7d34d2ce5d08b79b802b57edb27ecb4a055bc2603c2bb8e385f29b8268aa14f3b31a1c1f5fcfb422a79824c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalesSafari::Application.config.session_store :cookie_store, key: '_sales_safari_session'
